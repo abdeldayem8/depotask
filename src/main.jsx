9b8042f65ff2de361ded2fe73fff253405b42679
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-10-20 18:55:16 +0300

    change in route

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {  createHashRouter, RouterProvider } from 'react-router-dom'
 import Notfound from './Notfound.jsx'
 import Postdetail from './components/Postdetails/Postdetail.jsx'
 import Posts from './components/Posts/Posts.jsx'
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {path:'/',element:<App/>,
         children:[
             {path:'/',element:<Posts/>},
