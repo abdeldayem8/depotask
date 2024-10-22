6149ca15229a44f44847957530b5831b2c127865
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-10-22 15:09:46 +0300

    bu

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {   HashRouter, RouterProvider } from 'react-router-dom'
+import {   createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Notfound from './Notfound.jsx'
 import Postdetail from './components/Postdetails/Postdetail.jsx'
 import Posts from './components/Posts/Posts.jsx'
 
 
-const router = HashRouter([
+const router = createBrowserRouter([
     {path:'/',element:<App/>,
         children:[
             {path:'/',element:<Posts/>},
